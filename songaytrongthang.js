018568bdb3de9bf562bb0c7e63f8eb0151454fd4
TienThanhBN <tienthanhbn2006@gmail.com>
2024-10-10 10:49:30 +0700

    Update songaytrongthang.js

--- songaytrongthang.js
+++ songaytrongthang.js
@@ -1,4 +1,4 @@
-function soNgayTrongThang(thang, nam) {
+function soNgayTrongThang(thang) {
     let soNgay;
     if (thang === 1 || thang === 3 || thang === 5 || thang === 7 || thang === 8 || thang === 10 || thang === 12) {
         soNgay = 31;
@@ -7,18 +7,13 @@ function soNgayTrongThang(thang, nam) {
         soNgay = 30;
     }
     else if (thang === 2) {
-        if ((nam % 4 === 0 && nam % 100 !== 0) || (nam % 400 === 0)) {
-            soNgay = 29;
-        } else {
-            soNgay = 28;
-        }
+        soNgay = 28;
     }
     else {
         return "Số tháng không hợp lệ. Vui lòng nhập từ 1 đến 12.";
     }
-
     return `Tháng ${thang} có ${soNgay} ngày.`;
 }
 let thang = parseInt(prompt("Nhập số tháng (1-12):"));
-let nam = parseInt(prompt("Nhập năm:"));
-console.log(soNgayTrongThang(thang, nam));
+console.log(soNgayTrongThang(thang));
+
